b24f9e7cf21ae47ab69a41a137ec2ca319d3020f
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2023-04-10 19:05:57 -0500

    Back a Full!

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import usuarioRoutes from './routes/usuarioRoutes.js';
 
 const app = express();
 
 dotenv.config();
 connectDB();
+
+// Routing
+app.use('/api/usuarios', usuarioRoutes);
 
 const PORT = process.env.PORT || 4000;
 
