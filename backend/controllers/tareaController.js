8effff83d299ace5a7f03ed33e35728f882e9a4c
Sirio2022 <105917953+Sirio2022@users.noreply.github.com>
2023-04-14 08:00:45 -0500

    Vamos con tareas

---
+++ backend/controllers/tareaController.js
@@ -0,0 +1,17 @@
+const agregarTarea = async (req, res) => {};
+
+const obtenerTarea = async (req, res) => {};
+
+const editarTarea = async (req, res) => {};
+
+const eliminarTarea = async (req, res) => {};
+
+const cambiarEstadoTarea = async (req, res) => {};
+
+export {
+  agregarTarea,
+  obtenerTarea,
+  editarTarea,
+  eliminarTarea,
+  cambiarEstadoTarea,
+};
--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import usuarioRoutes from './routes/usuarioRoutes.js';
 import proyectoRoutes from './routes/proyectoRoutes.js';
+import tareaRoutes from './routes/tareaRoutes.js';
 
 const app = express();
 app.use(express.json());
@@ -13,6 +14,7 @@ connectDB();
 // Routing
 app.use('/api/usuarios', usuarioRoutes);
 app.use('/api/proyectos', proyectoRoutes);
+app.use('/api/tareas', tareaRoutes);
 
 const PORT = process.env.PORT || 4000;
 
